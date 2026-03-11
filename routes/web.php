bf59f33b320dc0d2bc36c727a904c1b92a1fcc47
akhil-stackcue <akhil.stackcue@gmail.com>
2026-03-11 23:13:56 +0530

    removed routes

--- routes/web.php
+++ routes/web.php
@@ -10,13 +10,9 @@ Route::get('/', function () {
     return redirect()->route('login');
 });
 
-Route::get('/da', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('old.dashboard');
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/da', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('old.dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
