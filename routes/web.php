79127fa9588ffd7719b59ce0d7034eaf072cd186
akhil-stackcue <akhil.stackcue@gmail.com>
2026-03-11 22:48:33 +0530

    added routes

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,10 @@ Route::get('/da', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('old.dashboard');
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
